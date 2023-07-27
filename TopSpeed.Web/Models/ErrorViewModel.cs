29912a29baf5a17cb46eaa46423c51722e6ee44f
Karthik <56579449+karthik9728@users.noreply.github.com>
2023-07-27 14:48:36 +0530

    Base Project File

---
+++ TopSpeed.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TopSpeed.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
