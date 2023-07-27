43568f565240cb85ce677564d960742922881a7d
Karthik <56579449+karthik9728@users.noreply.github.com>
2023-07-27 17:36:23 +0530

    Edit and Delete

--- TopSpeed.Web/Controllers/BrandController.cs
+++ TopSpeed.Web/Controllers/BrandController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using TopSpeed.Web.Data;
 using TopSpeed.Web.Models;
 
@@ -62,6 +63,114 @@ namespace TopSpeed.Web.Controllers
             }
 
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult Details(Guid id)
+        {
+            Brand brand = _dbContext.Brand.FirstOrDefault(x => x.Id == id);
+
+            return View(brand);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(Guid id)
+        {
+            Brand brand = _dbContext.Brand.FirstOrDefault(x => x.Id == id);
+
+            return View(brand);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Brand brand)
+        {
+            string webRootPath = _webHostEnvironment.WebRootPath;
+
+            var file = HttpContext.Request.Form.Files;
+
+            if (file.Count > 0)
+            {
+                string newFileName = Guid.NewGuid().ToString();
+
+                var upload = Path.Combine(webRootPath, @"images\brand");
+
+                var extension = Path.GetExtension(file[0].FileName);
+
+                //delete old image
+                var objFromDb = _dbContext.Brand.AsNoTracking().FirstOrDefault(x => x.Id == brand.Id);
+
+                if(objFromDb.BrandLogo != null)
+                {
+                    var oldImagePath = Path.Combine(webRootPath, objFromDb.BrandLogo.Trim('\\'));
+                    if (System.IO.File.Exists(oldImagePath))
+                    {
+                        System.IO.File.Delete(oldImagePath);
+                    }
+                }
+           
+
+                using (var fileStream = new FileStream(Path.Combine(upload, newFileName + extension), FileMode.Create))
+                {
+                    file[0].CopyTo(fileStream);
+                }
+
+                brand.BrandLogo = @"\images\brand\" + newFileName + extension;
+            }
+
+
+            if (ModelState.IsValid)
+            {
+                var objFromDb = _dbContext.Brand.AsNoTracking().FirstOrDefault(x => x.Id == brand.Id);
+
+                objFromDb.Name = brand.Name;
+                objFromDb.EstablishedYear = brand.EstablishedYear;
+
+                if(brand.BrandLogo != null)
+                {
+                    objFromDb.BrandLogo = brand.BrandLogo;
+                }
+
+                _dbContext.Brand.Update(objFromDb);
+                _dbContext.SaveChanges();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Delete(Guid id)
+        {
+            Brand brand = _dbContext.Brand.FirstOrDefault(x => x.Id == id);
+
+            return View(brand);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Brand brand)
+        {
+            string webRootPath = _webHostEnvironment.WebRootPath;
+
+            if(!string.IsNullOrEmpty(brand.BrandLogo))
+            {
+                //delete old image
+                var objFromDb = _dbContext.Brand.AsNoTracking().FirstOrDefault(x => x.Id == brand.Id);
+
+                if (objFromDb.BrandLogo != null)
+                {
+                    var oldImagePath = Path.Combine(webRootPath, objFromDb.BrandLogo.Trim('\\'));
+                    if (System.IO.File.Exists(oldImagePath))
+                    {
+                        System.IO.File.Delete(oldImagePath);
+                    }
+                }
+            }
+
+            _dbContext.Brand.Remove(brand);
+            _dbContext.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
