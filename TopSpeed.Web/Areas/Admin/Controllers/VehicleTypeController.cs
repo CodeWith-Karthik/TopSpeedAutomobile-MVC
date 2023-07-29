e9b86ae35e57ab7a0d2eb6adfd5d9efc0ca0777e
Karthik <56579449+karthik9728@users.noreply.github.com>
2023-07-29 17:37:02 +0530

    VehicleType Controller and Views

---
+++ TopSpeed.Web/Areas/Admin/Controllers/VehicleTypeController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TopSpeed.Application.ApplicationConstants;
+using TopSpeed.Application.Contracts.Presistence;
+using TopSpeed.Domain.Models;
+using TopSpeed.Infrastructure.Common;
+
+namespace TopSpeed.Web.Areas.Admin.Controllers
+{
+    public class VehicleTypeController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public VehicleTypeController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
+        {
+            _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            List<VehicleType> vehicleTypes = await _unitOfWork.VehicleType.GetAllAsync();
+
+            return View(vehicleTypes);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(VehicleType vehicleType)
+        {
+
+            if (ModelState.IsValid)
+            {
+                await _unitOfWork.VehicleType.Create(vehicleType);
+                await _unitOfWork.SaveAsync();
+
+                TempData["success"] = CommonMessage.RecordCreated;
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(Guid id)
+        {
+            VehicleType vehicleType = await _unitOfWork.VehicleType.GetByIdAsync(id);
+
+            return View(vehicleType);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            VehicleType vehicleType = await _unitOfWork.VehicleType.GetByIdAsync(id);
+
+            return View(vehicleType);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(VehicleType vehicleType)
+        {
+           
+            if (ModelState.IsValid)
+            {
+                await _unitOfWork.VehicleType.Update(vehicleType);
+                await _unitOfWork.SaveAsync();
+
+                TempData["warning"] = CommonMessage.RecordUpdated;
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            VehicleType vehicleType = await _unitOfWork.VehicleType.GetByIdAsync(id);
+
+            return View(vehicleType);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(VehicleType vehicleType)
+        {
+            
+            await _unitOfWork.VehicleType.Delete(vehicleType);
+            await _unitOfWork.SaveAsync();
+
+            TempData["error"] = CommonMessage.RecordDeleted;
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
